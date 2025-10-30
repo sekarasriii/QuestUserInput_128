d2cf1f0a29f7f856a93fa908afc9679fcdcb4fc8
sekarasriii <sekareeesh@gmail.com>
2025-10-30 16:00:33 +0700

    menambahkan deklrasi variabel untuk menyimpan data yang diperoleh dari komponen UI

--- app/src/main/java/com/example/meet5_inputdata/TeksEdit.kt
+++ app/src/main/java/com/example/meet5_inputdata/TeksEdit.kt
@@ -35,4 +35,8 @@ fun FormDataDiri(modifier: Modifier) {
     var textAlamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
 
+    // variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
 
