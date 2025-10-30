5a71af79d3d9e622436116d4df24fdc997f4d959
sekarasriii <sekareeesh@gmail.com>
2025-10-30 16:05:02 +0700

    membuat column utama untuk mengisi arrangement dan alignment

--- app/src/main/java/com/example/meet5_inputdata/TeksEdit.kt
+++ app/src/main/java/com/example/meet5_inputdata/TeksEdit.kt
@@ -41,3 +41,9 @@ fun FormDataDiri(modifier: Modifier) {
     var jenis by remember { mutableStateOf(value = "") }
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
